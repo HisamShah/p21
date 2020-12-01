09ededc97048d5b8b159624b64652b509c5b666c
HisamShah <62323860+HisamShah@users.noreply.github.com>
2020-12-01 19:48:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var bullet,speed,weight;
 function setup(){
   createCanvas(1600,400);
 
-  bullet = createSprite(50, 200, 50, 50);
+  bullet = createSprite(50, 200, 30, 20);
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor = color(80,80,80);
 
